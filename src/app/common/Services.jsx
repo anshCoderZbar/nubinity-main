579356c294f449246a70ce1458a241cc05567304
anshCoderZbar <ansh.coderzbar@gmail.com>
2024-05-28 09:40:58 +0530

    slider removed

--- src/app/common/Services.jsx
+++ src/app/common/Services.jsx
@@ -1,9 +1,4 @@
 import React from "react";
-import { Swiper, SwiperSlide } from "swiper/react";
-import { Pagination } from "swiper/modules";
-
-import "swiper/css";
-import "swiper/css/pagination";
 
 import { ServicesData } from "mock/Home";
 import { useState } from "react";
@@ -16,85 +11,36 @@ export const Services = () => {
   const [modalHeading, setModalHeading] = useState("");
   const [modalDesc, setModalDesc] = useState([]);
   return (
-    <>
-      <Swiper
-        slidesPerView={1}
-        spaceBetween={15}
-        loop={true}
-        modules={[Pagination]}
-        pagination={{ pagination: true, clickable: true }}
-        slidesPerGroup={1}
-        className="serrvices"
-        breakpoints={{
-          600: {
-            slidesPerView: 2,
-            slidesPerGroup: 2,
-          },
-          991: {
-            slidesPerView: 3,
-            slidesPerGroup: 3,
-          },
-          1400: {
-            slidesPerView: 4,
-            slidesPerGroup: 4,
-          },
-        }}
-      >
-        {ServicesData?.map((data, i) => {
-          return (
-            <SwiperSlide key={data?.id}>
-              <div data-toggle="modal" className="service_card">
-                <div
-                  onClick={() => {
-                    setIsOpen(true);
-                    setModalHeading(data?.heading1);
-                    setModalDesc(data?.list1);
-                  }}
-                  className="service-card-body"
-                >
-                  <img
-                    src={data.icon1}
-                    alt="service icons"
-                    data-aos="fade-up"
-                    data-aos-delay="500"
-                    data-aos-duration="1000"
-                  />
-                  <p
-                    data-aos="fade-up"
-                    data-aos-delay="500"
-                    data-aos-duration="1000"
-                  >
-                    {data?.heading1}
-                  </p>
-                </div>
-                <div
-                  onClick={() => {
-                    setIsOpen(true);
-                    setModalHeading(data?.heading2);
-                    setModalDesc(data?.list2);
-                  }}
-                  className="service-card-body"
-                >
-                  <img
-                    src={data.icon2}
-                    alt="service icons"
-                    data-aos="fade-up"
-                    data-aos-delay="500"
-                    data-aos-duration="1000"
-                  />
-                  <p
-                    data-aos="fade-up"
-                    data-aos-delay="500"
-                    data-aos-duration="1000"
-                  >
-                    {data?.heading2}
-                  </p>
-                </div>
-              </div>
-            </SwiperSlide>
-          );
-        })}
-      </Swiper>
+    <div className="servicees_container">
+      {ServicesData?.map((data, i) => {
+        return (
+          <div key={i} data-toggle="modal" className="service_card">
+            <div
+              onClick={() => {
+                setIsOpen(true);
+                setModalHeading(data?.heading);
+                setModalDesc(data?.list);
+              }}
+              className="service-card-body"
+            >
+              <img
+                src={data.icon}
+                alt="service icons"
+                data-aos="fade-up"
+                data-aos-delay="500"
+                data-aos-duration="1000"
+              />
+              <p
+                data-aos="fade-up"
+                data-aos-delay="500"
+                data-aos-duration="1000"
+              >
+                {data?.heading}
+              </p>
+            </div>
+          </div>
+        );
+      })}
       <Modal
         isOpen={isOpen}
         setIsOpen={setIsOpen}
@@ -146,132 +92,6 @@ export const Services = () => {
           </div>
         </div>
       </Modal>
-    </>
+    </div>
   );
 };
-{
-  /* <>
-<Swiper
-  slidesPerView={1}
-  spaceBetween={15}
-  loop={true}
-  modules={[Pagination]}
-  pagination={{ pagination: true, clickable: true }}
-  slidesPerGroup={1}
-  className="serrvices"
-  breakpoints={{
-    600: {
-      slidesPerView: 2,
-      slidesPerGroup: 2,
-    },
-    991: {
-      slidesPerView: 3,
-      slidesPerGroup: 3,
-    },
-    1400: {
-      slidesPerView: 4,
-      slidesPerGroup: 4,
-    },
-  }}
->
-  {ServicesData?.map((data) => {
-    return (
-      <SwiperSlide key={data?.id}>
-        <div
-          data-toggle="modal"
-          onClick={() => setIsOpen(true)}
-          className="service_card"
-        >
-          <div className="service-card-body">
-            <img
-              src={data.icon1}
-              alt="service icons"
-              data-aos="fade-up"
-              data-aos-delay="500"
-              data-aos-duration="1000"
-            />
-            <p
-              data-aos="fade-up"
-              data-aos-delay="500"
-              data-aos-duration="1000"
-            >
-              {data?.heading1}
-            </p>
-          </div>
-          <div className="service-card-body">
-            <img
-              src={data.icon2}
-              alt="service icons"
-              data-aos="fade-up"
-              data-aos-delay="500"
-              data-aos-duration="1000"
-            />
-            <p
-              data-aos="fade-up"
-              data-aos-delay="500"
-              data-aos-duration="1000"
-            >
-              {data?.heading2}
-            </p>
-          </div>
-        </div>
-      </SwiperSlide>
-    );
-  })}
-
-  <Modal
-    isOpen={isOpen}
-    setIsOpen={setIsOpen}
-    extraClass={"modal-dialog-centered"}
-  >
-    <div className="modal-body d-flex">
-      <div className="modal_text">
-        <h1
-          data-aos="fade-up"
-          data-aos-delay="500"
-          data-aos-duration="1000"
-        >
-          Managed Endpoint Protection
-        </h1>
-        <ul>
-          <li
-            data-aos="fade-up"
-            data-aos-delay="500"
-            data-aos-duration="1000"
-          >
-            Fully managed endpoint protection services designed to secure
-            devices against the evolving landscape of cyber threats. We
-            provide comprehensive defense and threat analytics for
-            laptops, desktops, and mobile devices, safeguarding them from
-            malware, ransomware, and sophisticated cyber-attacks.
-          </li>
-        </ul>
-        <a
-          href="https://clients.nubinity.com/"
-          target="_blank"
-          rel="noreferrer"
-          className="btn_bg"
-          data-aos="fade-up"
-          data-aos-delay="500"
-          data-aos-duration="1000"
-        >
-          Order
-        </a>
-      </div>
-      <div className="modal_img">
-        <img
-          src={privacyBanner}
-          alt="endpoint protection"
-          data-aos="zoom-in-right"
-          data-aos-delay="500"
-          data-aos-duration="1000"
-        />
-      </div>
-    </div>
-  </Modal>
-  <h4 className="text-center text-white slider_tetx">
-    Slide for more services
-  </h4>
-</Swiper>
-</> */
-}
--- src/mock/Home.js
+++ src/mock/Home.js
@@ -44,108 +44,132 @@ export const BannerData = [
 export const ServicesData = [
   {
     id: 1,
-    icon1: serviceIcon1,
-    icon2: serviceIcon01,
-    heading1: "Managed Endpoint Protection",
-    heading2: "Managed SSO",
-    list1: [
+    icon: serviceIcon1,
+    heading: "Managed Endpoint Protection",
+    list: [
       "Picture of an architecture diagram of the Elastic Stack or some other vendor agnostic endpoint protection related image.",
       "Fully managed endpoint protection services designed to secure devices against the evolving landscape of cyber threats. We provide comprehensive defense and threat analytics for laptops, desktops, and mobile devices, safeguarding them from malware, ransomware, and sophisticated cyber-attacks.",
-    ],
-    list2: [
-      "Managed SSO provides a seamless authentication experience for your organization, enabling users to access multiple applications with a single set of credentials. It increases security and simplifies the login process, reducing password fatigue and support tickets for lost or forgotten passwords.",
     ],
   },
   {
     id: 2,
-    icon1: serviceIcon2,
-    icon2: serviceIcon02,
-    heading1: "Managed Firewall Services",
-    heading2: "Professional Services",
-    list1: [
-      "We offer a comprehensive managed firewall solution, expertly designed to fortify your network's perimeter against cyber threats. Our service includes the deployment, configuration, and continuous management of advanced firewall solutions, tailored to meet your specific business needs. We offer cost effective support, so you only pay for the services and support you need.",
-      "This proactive approach not only enhances your overall security posture but also reduces the burden on your IT staff, allowing them to focus on strategic initiatives.",
-    ],
-    list2: [
-      "Our Professional Services offer expert consulting and implementation support for your IT projects. From initial strategy to final execution, we ensure that your technology investments are aligned with your business objectives, delivering custom solutions that drive efficiency and growth.",
+    icon: serviceIcon01,
+    heading: "Managed SSO",
+    list: [
+      "Managed SSO provides a seamless authentication experience for your organization, enabling users to access multiple applications with a single set of credentials. It increases security and simplifies the login process, reducing password fatigue and support tickets for lost or forgotten passwords.",
     ],
   },
   {
     id: 3,
-    icon1: serviceIcon3,
-    icon2: serviceIcon03,
-    heading1: "Managed Antivirus",
-    heading2: "Design Thinking Services",
-    list1: [
-      "We work with partners like Malwarebytes to bring you managed antivirus and anti-ransomware tools to protect desktops, servers, and mobile devices. When paired with our endpoint protection services, we can create an end-to-end solution that defends your critical systems from malware, web and email threats, browser-based attacks, and network-based intrusions.",
-    ],
-    list2: [
-      "Design Thinking Services are collaborative workshops aimed at solving complex problems through user-centric innovation. These sessions involve understanding user needs, ideating creative solutions, prototyping, and testing, all facilitated by our experienced design thinking experts.",
+    icon: serviceIcon2,
+    heading: "Managed Firewall Services",
+    list: [
+      "We offer a comprehensive managed firewall solution, expertly designed to fortify your network's perimeter against cyber threats. Our service includes the deployment, configuration, and continuous management of advanced firewall solutions, tailored to meet your specific business needs. We offer cost effective support, so you only pay for the services and support you need.",
+      "This proactive approach not only enhances your overall security posture but also reduces the burden on your IT staff, allowing them to focus on strategic initiatives.",
     ],
   },
   {
     id: 4,
-    icon1: serviceIcon4,
-    icon2: serviceIcon04,
-    heading1: "Security Consulting and Pentesting",
-    heading2: "Application and Web Design",
-    list1: [
-      "A comprehensive solution designed to identify and mitigate the vulnerabilities in your IT infrastructure. Our team of certified security professionals brings a wealth of experience in assessing and fortifying the cyber defenses of organizations across various industries. Through our meticulous penetration testing, we simulate real-world cyber-attacks to uncover potential weaknesses in your networks, applications, and systems. Alongside this, our consulting services provide strategic insights and tailored recommendations to enhance your security posture. We ensure that your organization is not only safeguarded against current threats but is also prepared for emerging cybersecurity challenges.",
-    ],
-    list2: [
-      "Application and Web Design services focus on creating user-friendly, aesthetically pleasing digital products. We prioritize intuitive navigation, responsive layouts, and engaging content to ensure your app or website effectively meets the needs of your audience. Our code is written by qualified, expert developers. We do not use auto-generated or AI generated code of any kind for client-facing projects.",
+    icon: serviceIcon02,
+    heading: "Professional Services",
+    list: [
+      "Our Professional Services offer expert consulting and implementation support for your IT projects. From initial strategy to final execution, we ensure that your technology investments are aligned with your business objectives, delivering custom solutions that drive efficiency and growth.",
     ],
   },
   {
     id: 5,
-    icon1: serviceIcon5,
-    icon2: serviceIcon05,
-    heading1: "cPanel Web Hosting",
-    heading2: "Reseller Plans",
-    list1: [
-      "cPanel Web Hosting provides a user-friendly web hosting solution, complete with an intuitive control panel for managing websites, databases, email accounts, and more. It's perfect for individuals and businesses looking for reliable and easily manageable hosting services.",
-    ],
-    list2: [
-      "Reseller Plans allow you to start your own hosting or consulting business. With our comprehensive tools and support, you can offer hosting services under your branding, manage multiple clients, and create an independent revenue stream.",
+    icon: serviceIcon3,
+    heading: "Managed Antivirus",
+    list: [
+      "We work with partners like Malwarebytes to bring you managed antivirus and anti-ransomware tools to protect desktops, servers, and mobile devices. When paired with our endpoint protection services, we can create an end-to-end solution that defends your critical systems from malware, web and email threats, browser-based attacks, and network-based intrusions.",
     ],
   },
   {
     id: 6,
-    icon1: serviceIcon6,
-    icon2: serviceIcon06,
-    heading1: "SSL Certificates",
-    heading2: "WISP Services",
-    list1: [
-      "SSL Certificates are digital certificates that help encrypt data transmitted between a user's browser and a website, ensuring secure online transactions. They are essential for protecting sensitive information and building trust with your customers.",
-    ],
-    list2: [
-      "WISP (Wireless Internet Service Provider) services offer high-speed internet solutions to customers through wireless broadband technology. Ideal for rural or underserved areas, our WISP services bridge the digital divide with reliable connectivity. We will work with you or your business to bring you fast, reliable internet access.",
+    icon: serviceIcon03,
+    heading: "Design Thinking Services",
+    list: [
+      "Design Thinking Services are collaborative workshops aimed at solving complex problems through user-centric innovation. These sessions involve understanding user needs, ideating creative solutions, prototyping, and testing, all facilitated by our experienced design thinking experts.",
     ],
   },
   {
     id: 7,
-    icon1: serviceIcon7,
-    icon2: serviceIcon07,
-    heading1: "Application Hosting",
-    heading2: "Website Builder",
-    list1: [
-      "Our cPanel based application hosting service offers a robust environment for deploying web applications. With high-performance servers, scalability, and 24/7 support, we ensure that your applications run smoothly and efficiently. Content delivery network services are also available. ",
-    ],
-    list2: [
-      "Our website builder is a drag-and-drop tool that makes it easy for anyone to create a professional-looking website without any coding knowledge. With customizable templates and intuitive design features, you can build and launch your site quickly.",
+    icon: serviceIcon4,
+    heading: "Security Consulting and Pentesting",
+    list: [
+      "A comprehensive solution designed to identify and mitigate the vulnerabilities in your IT infrastructure. Our team of certified security professionals brings a wealth of experience in assessing and fortifying the cyber defenses of organizations across various industries. Through our meticulous penetration testing, we simulate real-world cyber-attacks to uncover potential weaknesses in your networks, applications, and systems. Alongside this, our consulting services provide strategic insights and tailored recommendations to enhance your security posture. We ensure that your organization is not only safeguarded against current threats but is also prepared for emerging cybersecurity challenges.",
     ],
   },
   {
     id: 8,
-    icon1: serviceIcon8,
-    icon2: serviceIcon08,
-    heading1: "Storage, Email and DNS",
-    heading2: "Datacenter Services",
-    list1: [
+    icon: serviceIcon04,
+    heading: "Application and Web Design",
+    list: [
+      "Application and Web Design services focus on creating user-friendly, aesthetically pleasing digital products. We prioritize intuitive navigation, responsive layouts, and engaging content to ensure your app or website effectively meets the needs of your audience. Our code is written by qualified, expert developers. We do not use auto-generated or AI generated code of any kind for client-facing projects.",
+    ],
+  },
+  {
+    id: 9,
+    icon: serviceIcon5,
+    heading: "cPanel Web Hosting",
+    list: [
+      "cPanel Web Hosting provides a user-friendly web hosting solution, complete with an intuitive control panel for managing websites, databases, email accounts, and more. It's perfect for individuals and businesses looking for reliable and easily manageable hosting services.",
+    ],
+  },
+  {
+    id: 10,
+    icon: serviceIcon05,
+    heading: "Reseller Plans",
+    list: [
+      "Reseller Plans allow you to start your own hosting or consulting business. With our comprehensive tools and support, you can offer hosting services under your branding, manage multiple clients, and create an independent revenue stream.",
+    ],
+  },
+  {
+    id: 11,
+    icon: serviceIcon6,
+    heading: "SSL Certificates",
+    list: [
+      "SSL Certificates are digital certificates that help encrypt data transmitted between a user's browser and a website, ensuring secure online transactions. They are essential for protecting sensitive information and building trust with your customers.",
+    ],
+  },
+  {
+    id: 12,
+    icon: serviceIcon06,
+    heading: "WISP Services",
+    list: [
+      "WISP (Wireless Internet Service Provider) services offer high-speed internet solutions to customers through wireless broadband technology. Ideal for rural or underserved areas, our WISP services bridge the digital divide with reliable connectivity. We will work with you or your business to bring you fast, reliable internet access.",
+    ],
+  },
+  {
+    id: 13,
+    icon: serviceIcon7,
+    heading: "Application Hosting",
+    list: [
+      "Our cPanel based application hosting service offers a robust environment for deploying web applications. With high-performance servers, scalability, and 24/7 support, we ensure that your applications run smoothly and efficiently. Content delivery network services are also available.",
+    ],
+  },
+  {
+    id: 14,
+    icon: serviceIcon07,
+    heading: "Website Builder",
+    list: [
+      "Our website builder is a drag-and-drop tool that makes it easy for anyone to create a professional-looking website without any coding knowledge. With customizable templates and intuitive design features, you can build and launch your site quickly.",
+    ],
+  },
+  {
+    id: 15,
+    icon: serviceIcon8,
+    heading: "Storage, Email and DNS",
+    list: [
       "This service provides secure and scalable storage solutions, professional email hosting with your domain, and reliable Domain Name System (DNS) management to ensure your online presence is fast and always accessible.",
     ],
-    list2: [
-      "We provide secure, scalable, and redundant hosting solutions for your critical IT infrastructure. We can provide you with the necessary systems to connect your mission-critical applications to your customers by leveraging industry leading connectivity solutions such as Mikrotik and Cisco infrastructure, VMware NSX software defined network solutions, and managed configuration management and endpoint security appliances. ",
+  },
+  {
+    id: 16,
+    icon: serviceIcon08,
+    heading: "Datacenter Services",
+    list: [
+      "We provide secure, scalable, and redundant hosting solutions for your critical IT infrastructure. We can provide you with the necessary systems to connect your mission-critical applications to your customers by leveraging industry leading connectivity solutions such as Mikrotik and Cisco infrastructure, VMware NSX software defined network solutions, and managed configuration management and endpoint security appliances.",
     ],
   },
 ];
